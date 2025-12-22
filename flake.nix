981aa90135a0c00a1d293acf6e9e8b616d9d36d9
porkchop <danilkostin150705@gmail.com>
2025-12-23 01:17:02 +0300

    feat: create flake.nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "danilPorkchop15 NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    niri.url = "github:sodiboo/niri-flake";
+    niri.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, niri, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations.paragon = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+
+          niri.nixosModules.niri
+        ];
+      };
+    };
+}
